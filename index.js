31e1de91413fa2cc17912a13d13bf2d8f6854cd9
Ubuntu <ubuntu@ip-172-31-35-226.eu-west-1.compute.internal>
2016-10-16 10:08:23 +0000

    Port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var ms = require('./email/report_send.js');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/build'));
 
