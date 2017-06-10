72794f877340b8de4c274a4f9a362d005be18586
BootWP <burdianov@gmail.com>
2017-06-10 18:03:15 +0300

    Update

--- js/saveemail.js
+++ js/saveemail.js
@@ -56,3 +56,81 @@ function saveemail(email) {
 
 
 
+/* Validate Email */
+/*
+	$('[type="email"]').on('keypress', function (event) {
+		var regex = new RegExp("^[a-zA-Z0-9._@-]+$");
+		var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+		if (!regex.test(key)) {
+			event.preventDefault();
+			return false;
+		}
+	});
+	
+	$('[type="email"]').bind('paste', function (e) {
+		e.preventDefault();
+	});
+
+	$('.subscribe-form').each(function () {
+
+		var email = $('[type="email"]', this ).val();
+		console.log(email);
+		
+		$(this).validate({
+			rules:{
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			submitHandler: function(form) {
+				//$(form).serialize()
+				//form.addClass('form-disabled');
+				//alert('ok');
+				
+				$.ajax({
+					method: "POST",
+					url: 'https://platform.dao.casino/api/landing_subscribe.php',
+					data: 'email=email@mail.ru',// + $(form).serialize(),
+					success: function(data) {
+						
+						alert('Form Sent');
+						
+						//yaCounter42783759.reachGoal('EMAIL'); 
+						//ga('send', {hitType: 'event',eventCategory:'Form',eventAction: 'email', eventLabel: 'top'});
+						//$(' > [type="email"]', form ).val('');
+						//form.removeClass('form-disabled');
+						
+						$.magnificPopup.open({
+							items: {
+								src: '#open_thank_mail',
+								type: 'inline'
+							}
+						});
+						
+						
+					},
+					error:	function(data) {
+						alert('Server error! Contact support in intercom!');
+					},
+				});
+				
+				
+				$.ajax({
+					method: "POST",
+					url: 'https://platform.dao.casino/api/landing_subscribe2.php',
+					data: "email=" + email, //$(form).serialize(),
+					success: function(data) {
+						console.log('Form 2 Sent');
+					},
+					error:	function(data) {
+						console.log('Form 2 Error');
+					},
+				});
+			
+				
+				
+			}
+		});
+	});
+	*/
